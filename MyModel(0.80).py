bc09c6e3ac153495dd0db9df0844845c27f3c5d8
YangTian1710 <37905978+YangTian1710@users.noreply.github.com>
2018-06-15 16:38:06 +0800

    Add files via upload

---
+++ MyModel(0.80).py
@@ -0,0 +1,157 @@
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+from sklearn import metrics
+import sklearn.preprocessing as sp
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# In[ ]:
+
+
+train = pd.read_csv('../data/train_featureVB.csv')
+test = pd.read_csv('../data/test_featureVB.csv')
+
+# In[ ]:
+# 中文乱码处理
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+
+# 确保绘制的饼图为圆形
+plt.axes(aspect='equal')
+
+# 统计为风险用户的频数
+counts = train.label.value_counts()
+
+# 绘制饼图
+plt.pie(x=counts,
+        labels=pd.Series(counts.index).map({0: 'zero', 1: 'one'}),
+        autopct='%.2f%%')
+plt.show()
+
+X_train = train.drop(['uid','label'],axis=1)
+X_test = test.drop(['uid'],axis=1)
+
+# In[ ]:
+
+
+dtrain = lgb.Dataset(X_train,label=train.label)
+dtest = lgb.Dataset(X_test)
+
+# In[ ]:
+
+
+def evalMetric(preds, dtrain):
+    label = dtrain.get_label()
+
+    pre = pd.DataFrame({'preds': preds, 'label': label})
+    pre = pre.sort_values(by='preds', ascending=False)
+
+    auc = metrics.roc_auc_score(pre.label, pre.preds)
+
+    pre.preds = pre.preds.map(lambda x: 1 if x >= 0.5 else 0)
+
+    f1 = metrics.f1_score(pre.label, pre.preds)
+
+    res = 0.6 * auc + 0.4 * f1
+
+    return 'res', res, True
+
+
+# In[ ]:
+
+### 设置初始参数--不含交叉验证参数
+print('设置参数')
+params = {
+          'boosting_type': 'gbdt',
+          'objective': 'binary',
+          'metric': 'binary_logloss',
+		  'is_training_metric': False,
+		  'is_unbalance': True,
+          'min_data_in_bin': 45,
+          'num_leaves': 200,
+          'max_bin': 600,
+          'feature_fraction': 0.8,
+          'bagging_fraction': 0.8,
+          'verbosity': -1,
+          }
+
+### 交叉验证(调参)
+print('交叉验证')
+min_merror = float('Inf')
+best_params = {}
+
+# 准确率
+print("调参1：提高准确率")
+for num_leaves in range(20,200,5):
+    for max_depth in range(3,8,1):
+        params['num_leaves'] = num_leaves
+        params['max_depth'] = max_depth
+
+        cv_results = lgb.cv(
+                            params,
+                            dtrain,
+                            feval=evalMetric,
+                            seed=2018,
+                            nfold=3,
+                            metrics=['binary_error'],
+                            early_stopping_rounds=10,
+                            verbose_eval=True
+                            )
+
+        mean_merror = pd.Series(cv_results['binary_error-mean']).min()
+        boost_rounds = pd.Series(cv_results['binary_error-mean']).argmin()
+
+        if mean_merror < min_merror:
+            min_merror = mean_merror
+            best_params['num_leaves'] = num_leaves
+            best_params['max_depth'] = max_depth
+
+params['num_leaves'] = best_params['num_leaves']
+params['max_depth'] = best_params['max_depth']
+
+# ### 本地CV
+
+# In[ ]:
+
+
+lgb.cv(params, dtrain, feval=evalMetric, early_stopping_rounds=100, verbose_eval=5, num_boost_round=10000, nfold=3,
+       metrics=['evalMetric'])
+
+# ## 训练
+
+# In[ ]:
+
+
+model = lgb.train(params, dtrain,
+                  feval=evalMetric,
+                  verbose_eval=5, num_boost_round=300, valid_sets=[dtrain])
+
+# ### 预测
+
+# In[ ]:
+
+
+pred = model.predict(test.drop(['uid'], axis=1))
+
+# In[ ]:
+
+
+res = pd.DataFrame({'uid': test.uid, 'label': pred})
+
+# In[ ]:
+
+
+res = res.sort_values(by='label', ascending=False)
+res.label = res.label.map(lambda x: 1 if x >= 0.5 else 0)
+res.label = res.label.map(lambda x: int(x))
+
+# In[ ]:
+
+
+res.to_csv('../result/lgb-baseline0.75.csv', index=False, header=False, sep=',', columns=['uid', 'label'])
+
